152b2b41ef2de86f1562ce2da3d0e8ca14f8a088
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2022-03-27 22:39:55 -0300

    Add project files.

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+        }
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GravarUsandoEntity();
+            VisualizarUsandoEntity();
+            AlterarUsandoEntity();
+            VisualizarUsandoEntity();
+            DeletarUsandoEntity();
+            VisualizarUsandoEntity();
+            Console.ReadKey();
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAOEntity())
+            {
+                repo.Adicionar(p);
+            }
+        }
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var context = new ProdutoDAOEntity())
+            {
+                context.Adicionar(p);
+            }
+        }        
+        private static void VisualizarUsandoEntity()
+        {
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = contexto.Listar();
+                Console.WriteLine("Foram encontrados {0} produtos", produtos.Count);
+                foreach (var produto in produtos)
+                    Console.WriteLine(produto.Nome);
+            }
+        }
+        private static void DeletarUsandoEntity()
+        {
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = contexto.Listar();
+                foreach (var produto in produtos)
+                {
+                    contexto.Remover(produto);
+                }
+            }
+        }
+        private static void AlterarUsandoEntity()
+        {
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                Produto primeiro = contexto.Listar().First();
+                primeiro.Nome = "testeee";
+                contexto.Atualizar(primeiro);
+            }
+        }
+    }
+}
