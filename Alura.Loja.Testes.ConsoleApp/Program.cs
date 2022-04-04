fba013092056f3ce1385b262e2c03d852551b54e
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2022-04-04 00:24:43 -0300

    Adição de classe para visualizar código SQL e implementação de 1:N.

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,23 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            //Compra de 6 pães franceses
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão Francês";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
 
+            var compra = new Compra();
+            compra.Produto = paoFrances;
+            compra.Quantidade = 6;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Add(compra);
+                contexto.SaveChanges();
+            }
         }
     }
 }
