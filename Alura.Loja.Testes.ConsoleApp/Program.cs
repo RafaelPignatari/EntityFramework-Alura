4dd0bc2242b62b42152214a6fa5d761a2474d459
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2022-04-03 23:20:06 -0300

    Adição de Migrations.

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,68 +10,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoEntity();
-            VisualizarUsandoEntity();
-            AlterarUsandoEntity();
-            VisualizarUsandoEntity();
-            DeletarUsandoEntity();
-            VisualizarUsandoEntity();
-            Console.ReadKey();
-        }
 
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAOEntity())
-            {
-                repo.Adicionar(p);
-            }
-        }
-        private static void GravarUsandoEntity()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var context = new ProdutoDAOEntity())
-            {
-                context.Adicionar(p);
-            }
-        }        
-        private static void VisualizarUsandoEntity()
-        {
-            using (var contexto = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = contexto.Listar();
-                Console.WriteLine("Foram encontrados {0} produtos", produtos.Count);
-                foreach (var produto in produtos)
-                    Console.WriteLine(produto.Nome);
-            }
-        }
-        private static void DeletarUsandoEntity()
-        {
-            using (var contexto = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = contexto.Listar();
-                foreach (var produto in produtos)
-                {
-                    contexto.Remover(produto);
-                }
-            }
-        }
-        private static void AlterarUsandoEntity()
-        {
-            using (var contexto = new ProdutoDAOEntity())
-            {
-                Produto primeiro = contexto.Listar().First();
-                primeiro.Nome = "testeee";
-                contexto.Atualizar(primeiro);
-            }
         }
     }
 }
