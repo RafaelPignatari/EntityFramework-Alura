b8abe214bdc46d6f55b312445e0929baeda45a1a
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2022-04-05 20:25:01 -0300

    Adição de relacionamento N:N.

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -12,22 +12,30 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Compra de 6 pães franceses
-            var paoFrances = new Produto();
-            paoFrances.Nome = "Pão Francês";
-            paoFrances.PrecoUnitario = 0.40;
-            paoFrances.Unidade = "Unidade";
-            paoFrances.Categoria = "Padaria";
+            var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros"};
+            var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
+            var p3 = new Produto() { Nome = "Suco de Laranja", Categoria = "Alimentos", PrecoUnitario = 5, Unidade = "Litros" };
 
-            var compra = new Compra();
-            compra.Produto = paoFrances;
-            compra.Quantidade = 6;
-            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+            var promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Páscoa Feliz";
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
+
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
 
             using (var contexto = new LojaContext())
             {
-                contexto.Add(compra);
+                //Adiciona a promoção e todos os seus dependentes da tabela PromocaoProduto
+                //Nota: Não remove os produtos adicionados da tabela Produto.
+                contexto.Add(promocaoDePascoa);
                 contexto.SaveChanges();
+
+                //Remove o primeiro item da tabela Promocao.
+                //var promocao = contexto.Promocoes.FirstOrDefault();
+                //contexto.Remove(promocao);
+                //contexto.SaveChanges();
             }
         }
     }
